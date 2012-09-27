ad1b2327976b5f842625d36edcd8c2909763f004
Michael Lagace <veken0m.apps@gmail.com>
2012-09-27 11:39:38 -0400

    Fragments being added instead of replaced

--- src/com/veken0m/cavirtex/MainActivity.java
+++ src/com/veken0m/cavirtex/MainActivity.java
@@ -72,14 +72,12 @@ public class MainActivity extends SherlockFragmentActivity {
 
 		@Override
 		public void onTabReselected(Tab tab, FragmentTransaction ft) {
-			// ft.replace(R.id.virtexFragment, fragment);
-			// Toast.makeText(StartActivity.appContext, "Reselected!",
-			// Toast.LENGTH_LONG).show();
+			ft.replace(R.id.fragment_lay, fragment);
 		}
 
 		@Override
 		public void onTabSelected(Tab tab, FragmentTransaction ft) {
-			ft.add(R.id.fragment_lay, fragment);
+			ft.replace(R.id.fragment_lay, fragment);
 		}
 
 		@Override
