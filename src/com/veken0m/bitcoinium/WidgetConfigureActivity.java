c5cf6b873d6189baff816b99e2a5304ac1b904c7
Michael Lagace <veken0m.apps@gmail.com>
2013-01-31 11:52:41 -0500

    disable some more bitcoincentral stuff

--- src/com/veken0m/bitcoinium/WidgetConfigureActivity.java
+++ src/com/veken0m/bitcoinium/WidgetConfigureActivity.java
@@ -40,7 +40,7 @@ public class WidgetConfigureActivity extends PreferenceActivity {
 		((ListPreference) findPreference("virtexWidgetCurrencyPref")).setEnabled(false);
 		((ListPreference) findPreference("bitstampWidgetCurrencyPref")).setEnabled(false);
 		((ListPreference) findPreference("campbxWidgetCurrencyPref")).setEnabled(false);
-		((ListPreference) findPreference("bitcoincentralWidgetCurrencyPref")).setEnabled(false);
+		//((ListPreference) findPreference("bitcoincentralWidgetCurrencyPref")).setEnabled(false);
 		try{
 		((ListPreference) findPreference(widgetExchangePref.getEntry().toString().toLowerCase().replace("exchange", "") + "WidgetCurrencyPref")).setEnabled(true);
 		} catch (Exception e){
@@ -73,7 +73,7 @@ public class WidgetConfigureActivity extends PreferenceActivity {
         		((ListPreference) findPreference("virtexWidgetCurrencyPref")).setEnabled(false);
         		((ListPreference) findPreference("bitstampWidgetCurrencyPref")).setEnabled(false);
         		((ListPreference) findPreference("campbxWidgetCurrencyPref")).setEnabled(false);
-        		((ListPreference) findPreference("bitcoincentralWidgetCurrencyPref")).setEnabled(false);
+        		//((ListPreference) findPreference("bitcoincentralWidgetCurrencyPref")).setEnabled(false);
         		((ListPreference) findPreference(newValue.toString().toLowerCase().replace("exchange", "") + "WidgetCurrencyPref")).setEnabled(true);		
             	
                 return true;
