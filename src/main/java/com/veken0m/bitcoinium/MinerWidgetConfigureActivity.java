c9b58cf50b93baea1a13b4cf6f7035428b6ec316
Michael Lagacé <veken0m.apps@gmail.com>
2017-05-07 12:50:23 -0400

    Update widget configure activities
    - Use PreferencyFragment instead of PreferenceActivity

--- src/main/java/com/veken0m/bitcoinium/MinerWidgetConfigureActivity.java
+++ src/main/java/com/veken0m/bitcoinium/MinerWidgetConfigureActivity.java
@@ -5,16 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.Preference;
+import android.support.v7.app.AppCompatActivity;
 
 import com.veken0m.utils.Constants;
 
-public class MinerWidgetConfigureActivity extends PreferenceActivity implements Preference.OnPreferenceClickListener
+public class MinerWidgetConfigureActivity extends AppCompatActivity
 {
     private static final String PREF_MININGPOOL_KEY = "miningpool_";
-    private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
     public MinerWidgetConfigureActivity()
     {
@@ -26,7 +26,7 @@ public class MinerWidgetConfigureActivity extends PreferenceActivity implements 
     {
         SharedPreferences.Editor prefs = context.getSharedPreferences(Constants.PREFS_NAME_MINER, 0).edit();
         prefs.putString(PREF_MININGPOOL_KEY + appWidgetId, miningPool);
-        prefs.commit();
+        prefs.apply();
     }
 
     // Read the prefix from the SharedPreferences object for this widget.
@@ -37,30 +37,61 @@ public class MinerWidgetConfigureActivity extends PreferenceActivity implements 
         return prefs.getString(PREF_MININGPOOL_KEY + appWidgetId, null);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
+    public static class MinerWidgetConfigureFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener
     {
+        private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+        Context context;
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            context = getActivity().getApplicationContext();
+
+            addPreferencesFromResource(R.xml.pref_miner_widget);
+            addPreferencesFromResource(R.xml.pref_miner);
+
+            // Set the result to CANCELED. This will cause the widget host to cancel
+            // out of the widget placement if they press the back button.
+            getActivity().setResult(RESULT_CANCELED);
+
+            // Find the widget id from the intent.
+            Bundle extras = getActivity().getIntent().getExtras();
+            if (extras != null)
+                mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+
+            // If they gave us an intent without the widget id, just bail.
+            if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID)
+                getActivity().finish();
+
+            Preference OKpref = findPreference("OKpref");
+            OKpref.setOnPreferenceClickListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceClick(Preference preference)
+        {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            String pref_widgetMiningPool = prefs.getString("widgetMiningPoolPref", Constants.DEFAULT_MINING_POOL);
+
+            saveMiningPoolPref(context, mAppWidgetId, pref_widgetMiningPool);
+
+            // Make sure we pass back the original appWidgetId
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+            getActivity().setResult(RESULT_OK, resultValue);
+
+            getActivity().finish();
+            return true;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.pref_miner_widget);
-        addPreferencesFromResource(R.xml.pref_miner);
-        addPreferencesFromResource(R.xml.pref_widgets);
-        addPreferencesFromResource(R.xml.pref_create_widget);
 
-        // Set the result to CANCELED. This will cause the widget host to cancel
-        // out of the widget placement if they press the back button.
-        setResult(RESULT_CANCELED);
-
-        // Find the widget id from the intent.
-        Bundle extras = getIntent().getExtras();
-        if (extras != null)
-            mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-
-        // If they gave us an intent without the widget id, just bail.
-        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID)
-            finish();
-
-        Preference OKpref = findPreference("OKpref");
-        OKpref.setOnPreferenceClickListener(this);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new MinerWidgetConfigureFragment())
+                .commit();
     }
 
     @Override
@@ -69,22 +100,5 @@ public class MinerWidgetConfigureActivity extends PreferenceActivity implements 
         super.onStop();
 
         sendBroadcast(new Intent(this, MinerWidgetProvider.class).setAction(Constants.REFRESH));
-    }
-
-    @Override
-    public boolean onPreferenceClick(Preference preference)
-    {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String pref_widgetMiningPool = prefs.getString("widgetMiningPoolPref", Constants.DEFAULT_MINING_POOL);
-
-        saveMiningPoolPref(this, mAppWidgetId, pref_widgetMiningPool);
-
-        // Make sure we pass back the original appWidgetId
-        Intent resultValue = new Intent();
-        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-        setResult(RESULT_OK, resultValue);
-
-        finish();
-        return true;
     }
 }
