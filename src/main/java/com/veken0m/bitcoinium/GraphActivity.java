fc020d8a713dd7e824e114fab57f1eaa1c28044c
Michael Lagacé <veken0m.apps@gmail.com>
2017-05-07 12:27:54 -0400

    Update GraphView and ColorPickerPreference dependencies
    - Remove GraphView and ColorPickerPreference code
    - Fetch Graphview and ColorPickPreference depencies through gradle
    - Adapt GraphActivity to use new version of GraphView

--- src/main/java/com/veken0m/bitcoinium/GraphActivity.java
+++ src/main/java/com/veken0m/bitcoinium/GraphActivity.java
@@ -6,6 +6,8 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -21,9 +23,10 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
-import com.jjoe64.graphview.GraphView.GraphViewData;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.LineGraphView;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.veken0m.bitcoinium.exchanges.ExchangeProperties;
 import com.veken0m.bitcoinium.preferences.GraphPreferenceActivity;
 import com.veken0m.utils.Constants;
@@ -34,15 +37,15 @@ import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.Trade;
 import org.knowm.xchange.dto.marketdata.Trades;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import static com.veken0m.utils.ExchangeUtils.getDropdownItems;
 
 public class GraphActivity extends BaseActivity implements OnItemSelectedListener, SwipeRefreshLayout.OnRefreshListener
 {
-    private static final Handler mOrderHandler = new Handler();
+    private static final Handler mOrderHandler = new Handler(Looper.getMainLooper());
     private static Boolean connectionFail = true;
     private static Boolean noTradesFound = false;
 
@@ -56,7 +59,18 @@ public class GraphActivity extends BaseActivity implements OnItemSelectedListene
     /**
      * Variables required for LineGraphView
      */
-    private LineGraphView graphView = null;
+    private GraphView graphView = null;
+
+    private final Runnable mGetTrades = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            generatePriceGraph();
+        }
+    };
+
+
     /**
      * mGraphView run() is called when our GraphThread is finished
      */
@@ -162,6 +176,9 @@ public class GraphActivity extends BaseActivity implements OnItemSelectedListene
 
         try
         {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
             trades = ExchangeUtils.getMarketData(exchange).getTrades(currencyPair);
         }
         catch (Exception e)
@@ -171,59 +188,62 @@ public class GraphActivity extends BaseActivity implements OnItemSelectedListene
 
         try
         {
-            List<Trade> tradesList = new ArrayList<>();
-            if (trades != null) tradesList = trades.getTrades();
+            List<Trade> tradesList = trades.getTrades();
 
             float[] values = new float[tradesList.size()];
-            long[] dates = new long[tradesList.size()];
-            final GraphViewData[] data = new GraphViewData[values.length];
+            LineGraphSeries<DataPoint> data = new LineGraphSeries<>();
 
-            float largest = Integer.MIN_VALUE;
-            float smallest = Integer.MAX_VALUE;
-
+            Date date = tradesList.get(0).getTimestamp();
             final int tradesListSize = tradesList.size();
             for (int i = 0; i < tradesListSize; i++)
             {
-                final Trade trade = tradesList.get(i);
+                Trade trade = tradesList.get(i);
+                // if datapoints are less than 1 min apart, skip
+                if((trade.getTimestamp().getTime() - date.getTime() < 60000L) && i!=0)
+                    continue;
+
+                date = trade.getTimestamp();
                 values[i] = trade.getPrice().floatValue();
-                dates[i] = trade.getTimestamp().getTime();
-                if (values[i] > largest)
-                {
-                    largest = values[i];
-                }
-                if (values[i] < smallest)
-                {
-                    smallest = values[i];
-                }
-                data[i] = new GraphViewData(dates[i], values[i]);
+
+                data.appendData(new DataPoint(trade.getTimestamp(), values[i]),true,1000000);
             }
 
-            graphView = new LineGraphView(this, exchangeName + ": " + currencyPair.toString())
-            {
-                @Override
-                protected String formatLabel(double value, boolean isValueX)
-                {
-                    if (isValueX)
-                    {
-                        return Utils.dateFormat(getBaseContext(), (long) value);
-                    }
-                    else
-                        return super.formatLabel(value, false);
-                }
-            };
+            data.setThickness(10);
+            //data.setDrawDataPoints(true);
 
-            double windowSize = (dates[dates.length - 1] - dates[0]) / 2;
-            // startValue enables graph window to be aligned with latest
-            // trades
-            final double startValue = dates[dates.length - 1] - windowSize;
-            graphView.addSeries(new GraphViewSeries(data));
-            graphView.setViewPort(startValue, windowSize);
-            graphView.setScrollable(true);
-            graphView.setScalable(false);
+            graphView = new GraphView(this);
+            graphView.setTitle(exchangeName + ": " + currencyPair.toString());
+            graphView.setTitleTextSize(25);
+            graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+                @Override
+                public String formatLabel(double value, boolean isValueX) {
+                    return isValueX ? Utils.dateFormat(getBaseContext(), (long) value) : super.formatLabel(value, false);
+                }
+            });
+
+            graphView.addSeries(data);
+
+            //graphView.getGridLabelRenderer().setNumHorizontalLabels(4);
+            //graphView.getGridLabelRenderer().setNumVerticalLabels(10);
+            graphView.getGridLabelRenderer().setTextSize(18);
+
+            double largest = data.getHighestValueX();
+            double smallest = data.getLowestValueX();
+            double windowSize = (largest-smallest)/4;
+
+            graphView.getViewport().setXAxisBoundsManual(true);
+            graphView.getViewport().setMinX(smallest+windowSize);
+            graphView.getViewport().setMaxX(largest);
+
+            // enable scaling and scrolling
+            graphView.getViewport().setScalable(true);
+            graphView.getViewport().setScrollable(true);
 
             if (!pref_scaleMode)
             {
-                graphView.setManualYAxisBounds(largest, smallest);
+                graphView.getViewport().setYAxisBoundsManual(true);
+                graphView.getViewport().setMinY(data.getLowestValueY()*0.9999);
+                graphView.getViewport().setMaxY(data.getHighestValueY()*1.0001);
             }
             connectionFail = false;
             noTradesFound = false;
@@ -274,8 +294,8 @@ public class GraphActivity extends BaseActivity implements OnItemSelectedListene
     private void viewGraph()
     {
         swipeLayout.setRefreshing(true);
-        GraphThread gt = new GraphThread();
-        gt.start();
+        mOrderHandler.post(mGetTrades);
+        mOrderHandler.post(mGraphView);
     }
 
     @Override
@@ -351,27 +371,5 @@ public class GraphActivity extends BaseActivity implements OnItemSelectedListene
         graphLinearLayout.removeAllViews();
         viewGraph();
         //swipeLayout.setRefreshing(false);
-    }
-
-    private class GraphThread extends Thread
-    {
-        @Override
-        public void run()
-        {
-            generatePriceGraph();
-
-            /*
-            runOnUiThread(new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    LinearLayout linlaHeaderProgress = (LinearLayout) findViewById(R.id.graph_loadSpinner);
-                    linlaHeaderProgress.setVisibility(View.INVISIBLE);
-                }
-            });
-            */
-            mOrderHandler.post(mGraphView);
-        }
     }
 }
